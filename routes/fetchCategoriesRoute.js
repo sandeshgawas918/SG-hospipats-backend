7b5dec5ae15b502a95499c2c7976297439d2bc8e
sandesh gawas <sandeshgawas918@gmail.com>
2024-08-17 19:32:41 +0530

    updated fetch category

--- routes/fetchCategoriesRoute.js
+++ routes/fetchCategoriesRoute.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Category = require('../models/categoryModel.js')
 
 router.get('/fetchCategories',async(req,res)=>{
+    console.log('fetching categories')
     const allCategories=await Category.find().populate('categoryDoctor')
     res.send(allCategories)
 })

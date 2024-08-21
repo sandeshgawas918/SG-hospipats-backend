3754cec65e792c68bc43828b4464a0e63d96f52a
sandesh gawas <sandeshgawas918@gmail.com>
2024-08-21 23:01:10 +0530

    domain added

--- routes/loginUserRoute.js
+++ routes/loginUserRoute.js
@@ -17,14 +17,14 @@ router.post("/login", async (req, res) => {
     );
 
     if (matchedUser != null && comparePass) {
-       res.cookie("userId", matchedUser._id,
-        {
-          httpOnly: true,
-          secure: false,
-          // sameSite: "None",
-        }
-      );
-      res.send({ success: true, userEmail: matchedUser.email});
+      res.cookie("userId", matchedUser._id, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        domain: 'https://sg-hospipats-backend.onrender.com', // Set to your production domain
+        path: '/', // Set the path as needed
+      });
+      res.send({ success: true, userEmail: matchedUser.email });
     } else {
       console.log("else block -> matchedUser != null && comparePass");
       res.status(400).json({ success: false });

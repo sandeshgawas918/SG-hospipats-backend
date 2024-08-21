c2e54eb673bf57a99517d1f7adfb6b373b1a820c
sandesh gawas <sandeshgawas918@gmail.com>
2024-08-21 22:55:09 +0530

    http-true

--- routes/loginUserRoute.js
+++ routes/loginUserRoute.js
@@ -19,7 +19,7 @@ router.post("/login", async (req, res) => {
     if (matchedUser != null && comparePass) {
        res.cookie("userId", matchedUser._id,
         {
-          // httpOnly: true,
+          httpOnly: true,
           secure: false,
           // sameSite: "None",
         }

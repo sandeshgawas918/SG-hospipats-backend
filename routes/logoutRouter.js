335d576b89983e2c65588cf4bb10b3ef345984bd
sandesh gawas <sandeshgawas918@gmail.com>
2024-08-23 20:49:15 +0530

    cookie consoled

--- routes/logoutRouter.js
+++ routes/logoutRouter.js
@@ -3,6 +3,8 @@ const authorizationMiddleware = require('../middleware/protectRoute')
 const router = express.Router()
 
 router.get('/logout', (req, res) => {
+    const kk=req.cookies.userId
+    console.log(kk)
     res.clearCookie("userId")
     res.status(200).json({ message: 'Logout successful' });
 })

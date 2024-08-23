38dac6cb4fe5afbd561c505a16fe4a05bcbd7195
sandesh gawas <sandeshgawas918@gmail.com>
2024-08-23 21:15:21 +0530

    attributes added

--- routes/logoutRouter.js
+++ routes/logoutRouter.js
@@ -4,7 +4,12 @@ const router = express.Router()
 router.get('/logout', (req, res) => {
     const cookie = req.cookies.userId
     console.log(cookie)
-    res.clearCookie("userId")
+    res.clearCookie("userId", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        path: '/',
+    })
     res.status(200).json({ message: 'Logout successful' });
 })
 
